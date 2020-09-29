eb043bc93499e3c5720195451aa6c8eb579c3425
Hugo Solar <hugo.solar@gmail.com>
2020-09-28 22:54:43 -0300

    add script to equal card height

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
+jQuery(function($) {
+    function equalHeight(group) {
+        var tallest = 0;
+        group.each(function() {
+           var thisHeight = $(this).height();
+           if(thisHeight > tallest) {
+              tallest = thisHeight;
+           }
+        });
+        group.height(tallest);
+    }
+    equalHeight($(".equalheight"))
+});
 jQuery(document).ready(function($){
+  var equalHeight = function(group) {
+        var tallest = 0;
+        group.each(function() {
+           var thisHeight = $(this).height();
+           if(thisHeight > tallest) {
+              tallest = thisHeight;
+           }
+        });
+        group.height(tallest);
+    };
   $('.open-menu').on('click', function(e){
     e.preventDefault();
     $('.mobile-header').toggleClass('visible');
     $('.mobile-navigation').toggleClass('visible');
     return false;
   });
+   equalHeight($(".card"));
 });
